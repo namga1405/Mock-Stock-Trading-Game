ad254827565305afc4e09a8c043c7d2076324618
namga1405 <79681303+namga1405@users.noreply.github.com>
2021-09-08 15:30:39 +0700

    Add comments + methods that use encapsulation

--- stockAPI/src/main/java/Stonks/UserInfo.java
+++ stockAPI/src/main/java/Stonks/UserInfo.java
@@ -1,23 +1,66 @@
 package Stonks;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.sql.Statement;
+
 public class UserInfo {
-	private String username = null;
-	private String password = null;
+	int id;
+	String user;
+	int rank;
+	double balance;
+	static ArrayList<UserInfo> User;
 	
-	public UserInfo(String username, String password) {
-		this.password = password;
-		this.username = username;
-	}
+	//Create class
+	  public UserInfo(int id, String user, int rank, double balance){
+	        this.id = id;
+	        this.user = user;
+	        this.rank = rank;
+	        this.balance = balance;
+	        
+	    }
+	  
+	  //Set accessor method
+	  public int getId() {
+		  return id;
+	  }
+	  
+	  public String getUser() {
+		  return user;
+	  }
+	  
+	  public int getRank() {
+		  return rank;
+	  }
+	  
+	  public double getBalance() {
+		  return balance;
+	  }
+	  
+	//Method to set up a object to store the UserID, Username, Rank and Balance so it could be put on the table afterwards
+	public static ArrayList<UserInfo> UserList(){
+        ArrayList<UserInfo> UserList = new ArrayList<>();
+        try{
+        	//Connect to database, and then a query will be make to retrieve all information from the table ranking so it could be put into the ranking table via accessor methods
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/stocks","root","root");
+            String query = "SELECT * FROM ranking";
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            UserInfo user;
+            while(rs.next()){
+            	//Turn it into variable types that could be read in Eclipse then adding into the array list
+            	user = new UserInfo(rs.getInt("UserID"), rs.getString("Username"), rs.getInt("Rank"), rs.getDouble("Balance"));
+            	UserList.add(user);
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        
+        return UserList;
+    }
 	
-	public String getUsername() {
-		return username;
-	}
 	
-	public void setUsername(String username) {
-		this.username = username;
 	}
-	
-	public String getPassword() {
-		return password;
-	}
-}
