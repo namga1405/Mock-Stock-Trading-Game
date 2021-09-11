e48e0b4d6d0e53711a3c8011f078ae50f357817d
namga1405 <79681303+namga1405@users.noreply.github.com>
2021-09-11 15:24:37 +0700

    Re-organize

--- stockAPI/src/main/java/Stonks/UserInfo.java
+++ stockAPI/src/main/java/Stonks/UserInfo.java
@@ -7,10 +7,14 @@ import java.util.ArrayList;
 import java.sql.Statement;
 
 public class UserInfo {
-	int id;
-	String user;
-	int rank;
-	double balance;
+	private int id;
+	private String user;
+	private int rank;
+	private double balance;
+	private String stockname;
+	private int noBought;
+	private double cost;
+	private double value;
 	static ArrayList<UserInfo> User;
 	
 	//Create class
@@ -21,6 +25,13 @@ public class UserInfo {
 	        this.balance = balance;
 	        
 	    }
+	  
+	  public UserInfo(String stockname,int noBought, double cost, double value) {
+		  this.stockname = stockname;
+		  this.noBought = noBought;
+		  this.cost = cost;
+		  this.value = value;
+	  }
 	  
 	  //Set accessor method
 	  public int getId() {
@@ -39,6 +50,21 @@ public class UserInfo {
 		  return balance;
 	  }
 	  
+	  public String getStockname() {
+		  return stockname;
+	  }
+	  
+	  public int getNoBought() {
+		  return noBought;
+	  }
+	  
+	  public double getCost() {
+		  return cost;
+	  }
+	  
+	  public double getValue() {
+		  return value;
+	  }
 	//Method to set up a object to store the UserID, Username, Rank and Balance so it could be put on the table afterwards
 	public static ArrayList<UserInfo> UserList(){
         ArrayList<UserInfo> UserList = new ArrayList<>();
@@ -62,5 +88,117 @@ public class UserInfo {
         return UserList;
     }
 	
-	
+	//Method to get the userID given the username 
+	public static int getIDfromDB(String username) {
+		 int ID = 0;
+		 try{
+	        	//Connect to database, and then a query will be make to retrieve the userID of the chosen user from the table user
+	            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/stocks","root","root");
+	            String query = "SELECT UserID FROM user WHERE Username='"+username+"'";
+	            Statement st = con.createStatement();
+	            ResultSet rs = st.executeQuery(query);
+	            while(rs.next()){
+	            	//Turn it into variable types that could be read in Eclipse then adding into the array list
+	            	ID = rs.getInt("UserID");
+	            }
+	        }
+	        catch(Exception e){
+	            e.printStackTrace();
+	        }
+		 	return ID;
 	}
+	
+	//Method to get the number of stock bought in a transaction that a user with an ID i bought
+	public static ArrayList<Integer> getnoBought(int i){
+		 ArrayList<Integer> stocks = new ArrayList<>();
+		 try{
+	        	//Connect to database, and then a query will be make to retrieve required information in table transaction so it could be put into the ranking table via accessor methods
+	            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/stocks","root","root");
+	            String query = "SELECT (noBought) FROM transaction WHERE UserID ='"+i+"'";
+	            Statement st = con.createStatement();
+	            ResultSet rs = st.executeQuery(query);
+	            while(rs.next()) {
+	   	            	int noBought = rs.getInt("noBought");
+	   	            	stocks.add(noBought);    
+	            }
+	}
+		 catch(Exception exc) {
+				exc.printStackTrace();
+			}
+		 return stocks;
+	}
+	
+	//Method to get the cost of a transaction that a user with an ID i bought
+	public static ArrayList<Double> getCost(int i){
+		 ArrayList<Double> stocks = new ArrayList<>();
+		 try{
+	        	//Connect to database, and then a query will be make to retrieve required information in table transaction so it could be put into the ranking table via accessor methods
+	            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/stocks","root","root");
+	            String query = "SELECT (Cost) FROM transaction WHERE UserID ='"+i+"'";
+	            Statement st = con.createStatement();
+	            ResultSet rs = st.executeQuery(query);
+	            while(rs.next()) {
+	   	            	double cost = rs.getDouble("Cost");
+	   	            	stocks.add(cost);    
+	            }
+	}
+		 catch(Exception exc) {
+				exc.printStackTrace();
+			}
+		 return stocks;
+	}
+	
+	//Method to get the current value of a stock that a user with an ID i bought
+	public static ArrayList<Double> getValue(int i){
+		 ArrayList<Double> stocks = new ArrayList<>();
+		 try{
+	        	//Connect to database, and then a query will be make to retrieve required information in table transaction so it could be put into the ranking table via accessor methods
+	            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/stocks","root","root");
+	            String query = "SELECT (StockID) FROM transaction WHERE UserID ='"+i+"'";
+	            Statement st = con.createStatement();
+	            ResultSet rs = st.executeQuery(query);
+	            while(rs.next()) {
+	            	int ID = rs.getInt("StockID");
+	            	String query2 = "SELECT (Value) FROM stock WHERE StockID ='"+ID+"'";
+	   	            Statement st2 = con.createStatement();
+	   	            ResultSet rs2 = st2.executeQuery(query2);
+	   	            while(rs2.next()) {
+	   	            	double value = rs2.getDouble("Value");
+	   	            	stocks.add(value);    	
+	   	            }
+	            }
+	}
+		 catch(Exception exc) {
+				exc.printStackTrace();
+			}
+		 return stocks;
+	}
+	
+	//Method to get the stock name that a user with an ID i bought
+	public static ArrayList<String> getStockname(int i){
+		 ArrayList<String> stocks = new ArrayList<>();
+		 try{
+	        	//Connect to database, and then a query will be make to retrieve required information in table transaction so it could be put into the ranking table via accessor methods
+	            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/stocks","root","root");
+	            String query = "SELECT (StockID) FROM transaction WHERE UserID ='"+i+"'";
+	            Statement st = con.createStatement();
+	            ResultSet rs = st.executeQuery(query);
+	            while(rs.next()) {
+	            	int ID = rs.getInt("StockID");
+	            	String query2 = "SELECT (Name) FROM stock WHERE StockID ='"+ID+"'";
+	   	            Statement st2 = con.createStatement();
+	   	            ResultSet rs2 = st2.executeQuery(query2);
+	   	            while(rs2.next()) {
+	   	            	String stockname = rs2.getString("Name");
+	   	            	stocks.add(stockname);    	
+	   	            }
+	            }
+	}
+		 catch(Exception exc) {
+				exc.printStackTrace();
+			}
+		 return stocks;
+	}
+	
+	
+}
