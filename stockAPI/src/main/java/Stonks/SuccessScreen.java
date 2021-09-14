71c4ce7b1890e946bed5a5a77a18d814be9f5a6e
namga1405 <79681303+namga1405@users.noreply.github.com>
2021-09-14 10:36:31 +0700

    Update SuccessScreen.java

--- stockAPI/src/main/java/Stonks/SuccessScreen.java
+++ stockAPI/src/main/java/Stonks/SuccessScreen.java
@@ -14,8 +14,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -58,35 +60,50 @@ public class SuccessScreen extends JFrame {
 	}
 	
 	//Get stocks from the database with selected country to put to array list
-	private ArrayList<String> getStocks(Object object) {
+	private ArrayList<String> getStocks(int i) {
 		ArrayList<String> stocks = new ArrayList<String>();
-		String i = object.toString();
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection com = DriverManager.getConnection("jdbc:mysql://localhost:3306/stocks","root","root");
 			Statement stmt = com.createStatement();
 			//Retrieve marketID with where the name is matched on the combo box
-			String retrieveID = "SELECT MarketID FROM market WHERE marketName='"+i+"'";
-			ResultSet rs = stmt.executeQuery(retrieveID);
-			//Continue adding until there's non left
-			while (rs.next()) {
-				//Getting the ID from the market DB to query for the stock DB since the ID is essentially acting like a foreign key acessor
-				int id = rs.getInt("MarketID");
-				//Finding the list of stocks with matched ID
-				String retrieveStock = "Select Name FROM stock WHERE MarketID='"+id+"'";
+				String retrieveStock = "Select Name FROM stock WHERE MarketID='"+i+"'";
 				ResultSet rs2 = stmt.executeQuery(retrieveStock);
 				while (rs2.next()) {
 					String name = rs2.getString("Name");
 					//Adding it to the array list
 					stocks.add(name);
 				}
-			}
 		}
 		catch (Exception exception) {
 			exception.printStackTrace();
 		}
 		return stocks;
 	}
+	
+	private int getMarketID(Object Object) {
+		int i = 0;
+		String s = Object.toString();
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection com = DriverManager.getConnection("jdbc:mysql://localhost:3306/stocks","root","root");
+			Statement stmt = com.createStatement();
+			//Retrieve marketID with where the name is matched on the combo box
+			String retrieveID = "SELECT MarketID FROM market WHERE marketName='"+s+"'";
+			ResultSet rs = stmt.executeQuery(retrieveID);
+			//Continue adding until there's non left
+			while (rs.next()) {
+				//Getting the ID from the market DB to query for the stock DB since the ID is essentially acting like a foreign key acessor
+				i = rs.getInt("MarketID");
+			}
+		}
+			catch (Exception exception) {
+				exception.printStackTrace();
+			}
+		return i;
+	}
+	
+	
 	
 	private double getPrice(String s) {
 		double price = 0.00;
@@ -98,8 +115,7 @@ public class SuccessScreen extends JFrame {
 			chosen stockname on the table stock*/
 			String retrieveprice = "SELECT Value FROM stock WHERE Name='"+s+"'";
 			ResultSet rs = stmt.executeQuery(retrieveprice);
-			
-			if (rs.next()) {
+			while(rs.next()) {
 				//Return value to the program
 				price = rs.getDouble("Value");
 			}
@@ -114,6 +130,52 @@ public class SuccessScreen extends JFrame {
 	private String[] convertArray(ArrayList<String> a) {
 		String[] array = a.toArray(new String[0]);
 		return array;
+	}
+	
+	public int getStockIDfromDB(String s) {
+		 int ID = 0;
+		 try{
+	        	//Connect to database, and then a query will be make to retrieve the userID of the chosen user from the table user
+	            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/stocks","root","root");
+	            String retrieveID = "SELECT StockID FROM stock WHERE Name='"+s+"'";
+	            Statement st = con.createStatement();
+	            ResultSet rs = st.executeQuery(retrieveID);
+	            while(rs.next()){
+	            	//Turn it into variable types that could be read in Eclipse then adding into the array list
+	            	ID = rs.getInt("StockID");
+	            }
+	        }
+	        catch(Exception e){
+	            e.printStackTrace();
+	        }
+		 	return ID;
+	}	
+	
+	private void pushtoDB(int noBought, double cost, String boughtDate, int user, int stock) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection com = DriverManager.getConnection("jdbc:mysql://localhost:3306/stocks","root","root");
+			Statement stmt = com.createStatement();
+			String pushDB = "INSERT INTO transaction(noBought,Cost,boughtDate,UserID,StockID) values('"+noBought+"','"+cost+"','"+boughtDate+"','"+user+"','"+stock+"')";
+			int x = stmt.executeUpdate(pushDB);
+			if (x == 0) {
+				System.out.println("Addition failed");
+			} else {
+				//Confirmation
+				System.out.println("Added");
+				}
+		}
+		catch(Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+
+	
+	public String getDate() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+		Date date = new Date();
+		String time = sdf.format(date);
+		return time;
 	}
 		
 	private void timeuntilClosing() {
@@ -174,7 +236,8 @@ public class SuccessScreen extends JFrame {
 		cmbMarket.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				cmbStocks.removeAllItems();
-				storeStock = getStocks(cmbMarket.getSelectedItem());
+				int marketID = getMarketID(cmbMarket.getSelectedItem());
+				storeStock = getStocks(marketID);
 				//Change it to an array since it is required to make the the model
 				String[] stockname = convertArray(storeStock);
 				final DefaultComboBoxModel Model = new DefaultComboBoxModel(stockname);
@@ -186,23 +249,23 @@ public class SuccessScreen extends JFrame {
 		JLabel lblStockName_1 = new JLabel("Quantity");
 		lblStockName_1.setHorizontalAlignment(SwingConstants.CENTER);
 		lblStockName_1.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblStockName_1.setBounds(310, 257, 98, 24);
+		lblStockName_1.setBounds(10, 268, 98, 24);
 		contentPane.add(lblStockName_1);
 		
 		txtQuantity = new JTextField();
-		txtQuantity.setBounds(419, 259, 138, 24);
+		txtQuantity.setBounds(97, 270, 138, 24);
 		contentPane.add(txtQuantity);
 		txtQuantity.setColumns(10);
 		
 		JLabel lbl = new JLabel("Total:");
 		lbl.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lbl.setBounds(35, 257, 75, 24);
+		lbl.setBounds(32, 233, 75, 24);
 		contentPane.add(lbl);
 		
 		final JLabel lblPrice = new JLabel("Price");
 		lblPrice.setForeground(Color.RED);
 		lblPrice.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblPrice.setBounds(87, 252, 111, 34);
+		lblPrice.setBounds(87, 228, 490, 34);
 		contentPane.add(lblPrice);
 		
 		JLabel lblNewLabel_2 = new JLabel("Window close in:");
@@ -238,17 +301,51 @@ public class SuccessScreen extends JFrame {
 			}
 		});
 		
-		JButton btnConfirm = new JButton("Confirm");
-		btnConfirm.setForeground(Color.RED);
-		btnConfirm.setFont(new Font("SansSerif", Font.BOLD, 15));
-		btnConfirm.setBounds(478, 417, 130, 34);
-		contentPane.add(btnConfirm);
 		
 		final JComboBox cmbCurrency = new JComboBox();
 		cmbCurrency.setFont(new Font("Tahoma", Font.BOLD, 15));
 		cmbCurrency.setModel(new DefaultComboBoxModel(new String[] {"", "USD", "CAD", "AUD", "EUR", "VND"}));
 		cmbCurrency.setBounds(439, 106, 138, 34);
 		contentPane.add(cmbCurrency);
+		
+		JButton btnConfirm = new JButton("Confirm");
+		btnConfirm.setForeground(Color.RED);
+		btnConfirm.setFont(new Font("SansSerif", Font.BOLD, 15));
+		btnConfirm.setBounds(478, 417, 130, 34);
+		contentPane.add(btnConfirm);
+		btnConfirm.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//Retrieve the price from the selected stocks by using the method getPrice
+				String stockname = (String) cmbStocks.getSelectedItem();
+				double stockprice = getPrice((String) cmbStocks.getSelectedItem());
+				int ID = UserInfo.getIDfromDB(username);
+				double balance = UserInfo.getBalancefromID(ID);
+				//Get the quantity from user input
+				String q = txtQuantity.getText();
+				int quantity = Integer.valueOf(q);
+				int stockID = getStockIDfromDB(stockname);
+				//Calculate the total + Round it to 2 decimal places
+				double total = quantity*stockprice;
+				double currentBalance = balance - total;
+				String time = getDate();
+				//If the cost of the purchase is larger than the balance then there will be a pane telling that the purchase is not progressed
+				if (total > balance) {
+					JOptionPane.showMessageDialog(null, "Fund not sufficient, automatically return to menu");
+				}
+				else {
+					//If the cost of the purchase is smaller, then all of the required information is being updated to the transaction table and update the balance in the ranking table(without changing the actual value of the rank)
+					pushtoDB(quantity,total,time,ID,stockID);
+					UserInfo.updateBalancetoRanking(ID,currentBalance);
+					JOptionPane.showMessageDialog(null, "Congratz you bought some stonks!, automatically return to menu");
+				}
+				//Return to menu screen
+				setVisible(false);
+				menuScreen menu = new menuScreen();
+				menu.showName(username);
+				menu.setVisible(true);
+			}
+		});
+
 		
 		JLabel lblNewLabel_1 = new JLabel("Country");
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
